bc92866ef77d7c055df69b15746012226e510b59
takashyx <takashyx@gmail.com>
2021-04-02 07:53:32 +0900

    Add graph

--- monitor_CO2_cplus.py
+++ monitor_CO2_cplus.py
@@ -1,4 +1,3 @@
-import wifiCfg
 from m5stack import *
 import machine
 import gc
@@ -6,11 +5,50 @@ import utime
 import uos
 import _thread
 import math
-import time
+
+
+# graph class ring buffer and draw
+class GraphData:
+    def __init__(self, size):
+        self.buffer = [0 for i in range(0, size)]
+        self.start = 0
+        self.end = size - 1
+        self.size = size
+
+    def log(self, val):
+        self.buffer[self.end] = val
+        self.end = (self.end + 1) % len(self.buffer)
+
+    def get(self):
+        val = self.buffer[self.start]
+        self.start = (self.start + 1) % len(self.buffer)
+        return val
+
+    def __len__(self):
+        return self.end - self.start
+
+    # lcd x(graph height) 100 20ppm=1dot
+    # lcd y(graph width)  100  10 sec=1dot
+    def draw_graph(self, x, y):
+        for i in range(0, self.size):
+            val = self.buffer[i]
+            # set line color from co2 value
+            if val > CO2_RED:
+                col = lcd.RED
+            elif val > CO2_YELLOW:
+                col = lcd.YELLOW
+            else:
+                col = lcd.WHITE
+
+            # draw graph
+            if disp_mode == 1:
+                lcd.line(x - 100, y + i, x - 100 + int(val // 20), y + i, col)
+            else:
+                lcd.line(x + 100, y - i, x + 100 - int(val // 20), y - i, col)
 
 
 # 変数宣言
-Disp_mode = 0     # グローバル
+disp_mode = 0     # グローバル
 lcd_mute = False  # グローバル
 data_mute = False  # グローバル
 co2_interval = 1     # MH-19B/Cへco2測定値要求コマンドを送るサイクル（秒）
@@ -21,8 +59,14 @@ co2 = 0
 co2_str = '---'
 preheat_count = 60  # センサー安定後数値が取れるようになるまでの時間 MH-19B :180 MH-19C:60
 
+preheat_status = ""
+preheat_status_fc = lcd.BLACK
+
+co2_graph_data = GraphData(100)
 
 # @cinimlさんのファーム差分吸収ロジック
+
+
 class AXPCompat(object):
     def __init__(self):
         if(hasattr(axp, 'setLDO2Vol')):
@@ -35,10 +79,10 @@ axp = AXPCompat()
 
 
 # PWM用パルス関数
-def pulse(l, t):
+def pulse(length, time):
     for i in range(100):
-        l.duty(int((math.sin(i / 50 * math.pi) * 50) + 50))
-        time.sleep_ms(t)
+        length.duty(int((math.sin(i / 50 * math.pi) * 50) + 50))
+        utime.sleep_ms(time)
 
 
 # LED PWMスレッド関数
@@ -51,16 +95,16 @@ def threadfunc_led_controller():
     g10 = machine.PWM(machine.Pin(10), frequency)
     for i in range(3):
         g10.duty(0)
-        time.sleep_ms(100)
+        utime.sleep_ms(100)
         g10.duty(100)
-        time.sleep_ms(100)
+        utime.sleep_ms(100)
 
     while True:
         if co2 >= CO2_RED and not lcd_mute:
             pulse(g10, 10)
         else:
             g10.duty(100)
-            time.sleep_ms(100)
+            utime.sleep_ms(100)
 
 
 # 表示OFFボタン処理スレッド関数
@@ -80,53 +124,46 @@ def buttonA_wasPressed():
 
 # 表示切替ボタン処理スレッド関数
 def buttonB_wasPressed():
-    global Disp_mode
-    if Disp_mode == 1:
-        Disp_mode = 0
+    global disp_mode
+    if disp_mode == 1:
+        disp_mode = 0
     else:
-        Disp_mode = 1
+        disp_mode = 1
 
-    lcd_clear()
-    draw_co2()
+    draw()
 
 
 def threadfunc_preheat_timer_count():
-    global Disp_mode
+    global disp_mode
+    global preheat_status
     global preheat_count
+    global preheat_status_fc
+    global preheat_status
 
-    lcd.clear()
     while True:
         if preheat_count > 0:
-            fc = lcd.YELLOW
-            status = "Preheating... " + str(preheat_count)
+            preheat_status_fc = lcd.YELLOW
+            preheat_status = "Preheating... " + str(preheat_count)
             preheat_count = preheat_count - 1
         else:
-            fc = lcd.WHITE
-            status = "OK"
+            preheat_status_fc = lcd.WHITE
+            preheat_status = "OK"
+            break
 
-        if Disp_mode == 1:  # 表示回転処理
-            lcd.rect(110, 0, 135, 240, lcd.BLACK, lcd.BLACK)
-            lcd.line(111, 0, 111, 240, lcd.LIGHTGREY)
-            lcd.font(lcd.FONT_DejaVu18, rotate=90)
-            lcd.print(status, 131, 30, fc)
-        else:
-            lcd.rect(0, 0, 24, 240, lcd.BLACK, lcd.BLACK)
-            lcd.line(24, 0, 24, 240, lcd.LIGHTGREY)
-            lcd.font(lcd.FONT_DejaVu18, rotate=270)
-            lcd.print(status, 4, 210, fc)
-        utime.sleep(1)
+        utime.sleep_ms(100)
+
 
 # 表示モード切替時の枠描画処理関数
-
-
-def draw_co2():
-    global Disp_mode
+def draw():
+    global disp_mode
     global lcd_mute
     global data_mute
     global CO2_RED
     global CO2_YELLOW
     global co2
     global co2_str
+    global preheat_status
+    global preheat_status_fc
 
     if data_mute or (co2 == 0):  # タイムアウトで表示ミュートされてるか、初期値のままならco2値非表示（灰文字化）
         fc = lcd.LIGHTGREY
@@ -143,18 +180,45 @@ def draw_co2():
     else:
         co2_str = str(co2)
 
+    # clear
+    lcd.fillScreen(lcd.BLACK)
 
-    if Disp_mode == 1:  # 表示回転処理
-        lcd.rect(0, 0, 111, 240, lcd.BLACK, lcd.BLACK)
+    if disp_mode == 1:  # 表示回転処理
+        # status
+        lcd.font(lcd.FONT_DejaVu18, rotate=90)
+        lcd.print(preheat_status, 131, 30, preheat_status_fc)
+
+        # line
+        lcd.line(111, 0, 111, 240, lcd.LIGHTGREY)
+
+        # "CO2 ppm" string
         lcd.font(lcd.FONT_DejaVu18, rotate=90)  # 単位(ppm)の表示
         lcd.print('CO2 ppm', 103, 130, fc)
+
+        # draw graph
+        co2_graph_data.draw_graph(100, 0)
+
+        # CO2 value
         lcd.font(lcd.FONT_DejaVu72, rotate=90)  # co2値の表示
         co2_str_w = int(lcd.textWidth(co2_str))
         lcd.print(co2_str, 70, (210 - co2_str_w), fc)
+
     else:
-        lcd.rect(25, 0, 135, 240, lcd.BLACK, lcd.BLACK)
+        # status
+        lcd.font(lcd.FONT_DejaVu18, rotate=270)
+        lcd.print(preheat_status, 4, 210, preheat_status_fc)
+
+        # line
+        lcd.line(24, 0, 24, 240, lcd.LIGHTGREY)
+
+        # "CO2 ppm" string
         lcd.font(lcd.FONT_DejaVu18, rotate=270)  # 単位(ppm)の表示
         lcd.print('CO2 ppm', 32, 110, fc)
+
+        # draw graph
+        co2_graph_data.draw_graph(80, 240)
+
+        # CO2 value
         lcd.font(lcd.FONT_DejaVu72, rotate=0)  # co2値の表示
         co2_str_w = int(lcd.textWidth(co2_str))
         lcd.font(lcd.FONT_DejaVu72, rotate=270)  # co2値の表示
@@ -216,7 +280,7 @@ def co2_set_filechk():
 
 
 # 画面初期化
-axp.setLDO2Vol(2.6)  # バックライト輝度調整（中くらい）
+axp.setLDO2Vol(2.8)  # バックライト輝度調整（中くらい）
 
 # ユーザー設定ファイル読み込み
 co2_set_filechk()
@@ -236,14 +300,13 @@ btnB.wasPressed(buttonB_wasPressed)
 # タイムカウンタ初期値設定
 co2_tc = utime.time()
 
-# LED表示スレッド起動
-_thread.start_new_thread(threadfunc_led_controller, ())
-
-# タイマー表示スレッド起動
+# preheatタイマー表示スレッド起動
 _thread.start_new_thread(threadfunc_preheat_timer_count, ())
 
+# 警告赤色LEDスレッド起動
+_thread.start_new_thread(threadfunc_led_controller, ())
 
-# メインルーチン
+# メインルーチン（初期設定と描画ループ）
 while True:
     if (utime.time() - co2_tc) >= co2_interval:  # co2要求コマンド送信
         mhz19b_data = bytearray(9)
@@ -256,13 +319,13 @@ while True:
                 mhz19b_data):    # 応答かどうかの判定とチェックサムチェック
             co2_tc = utime.time()
             co2 = mhz19b_data[2] * 256 + mhz19b_data[3]
+            co2_graph_data.log(co2)
             data_mute = False
-            draw_co2()
-        utime.sleep(1)
+            draw()
 
     if (utime.time() - co2_tc) >= TIMEOUT:  # co2応答が一定時間無い場合はCO2値表示のみオフ
         data_mute = True
-        draw_co2()
+        draw()
 
-    utime.sleep(0.2)
+    utime.sleep_ms(100)
     gc.collect()
